bb0a8c144326777d68eca73ed3848207238bd8b4
RomaSamkov <Romansamkov.rs@gmail.com>
2022-09-10 21:40:25 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,41 +1,23 @@
 const express = require("express");
-
-const contactsOperations = require("../../models/contacts");
+const { schemaValidation } = require("../../middlewares/validationMiddleware");
+const {
+  getAllContacts,
+  getContactById,
+  addContact,
+  deleteContact,
+  updateContact,
+} = require("../../controllers/contactControllers");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  const contacts = await contactsOperations.listContacts();
-  res.status(200).json({ status: "success", code: 200, data: contacts });
-});
+router.get("/", getAllContacts);
 
-router.get("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await contactsOperations.getContactById(contactId);
-  contact
-    ? res.status(200).json({ status: "success", data: contact })
-    : res.status(404).json({ message: "Not found" });
-});
+router.get("/:contactId", getContactById);
 
-router.post("/", async (req, res, next) => {
-  const contact = await contactsOperations.addContact(req.body);
-  res.status(201).json({ status: "success", data: contact });
-});
+router.post("/", schemaValidation, addContact);
 
-router.delete("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await contactsOperations.removeContact(contactId);
-  contact
-    ? res.status(200).json({ message: "contact deleted" })
-    : res.status(404).json({ message: "Not found" });
-});
+router.delete("/:contactId", deleteContact);
 
-router.put("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await contactsOperations.updateContact(contactId, req.body);
-  contact
-    ? res.status(200).json({ data: contact })
-    : res.status(404).json({ message: "Not found" });
-});
+router.put("/:contactId", schemaValidation, updateContact);
 
 module.exports = router;
