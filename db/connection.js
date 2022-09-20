ab9a3c61f6a1ccddde70a0333a591dee3338f4af
RomaSamkov <Romansamkov.rs@gmail.com>
2022-09-20 23:19:26 +0300

    Create connection.js

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const url = process.env.DB_HOST;
+
+const connection = async () => {
+  return mongoose.connect(url, {
+    promiseLibrary: global.Promise,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = {
+  connection,
+};
