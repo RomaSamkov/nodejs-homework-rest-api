a4af342aa28c357c1162fcc1586bb84e530fdee0
RomaSamkov <Romansamkov.rs@gmail.com>
2022-09-21 23:24:16 +0300

    Create errorHandler.js

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,11 @@
+function handlerCatchErrors(middleware) {
+  return async function (req, res, next) {
+    try {
+      await middleware(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+module.exports = handlerCatchErrors;
