6241304b95d5e9d683ab0fbd532689442c15dede
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-04 23:31:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const createError = require("http-errors");
 const handleCatchErrors = require("./middlewares/errorHandler");
 
 const contactsRouter = require("./routes/api/contacts");
+const authRouter = require("./routes/api/auth");
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", authRouter);
 
 app.use(
   handleCatchErrors((req, res, next) => {
