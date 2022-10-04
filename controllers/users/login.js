4ca809c6cea9ed764ab7ba40c8483a6c9c500305
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-04 23:31:11 +0300

    Create login.js

---
+++ controllers/users/login.js
@@ -0,0 +1,31 @@
+const service = require("../../service/users");
+const createError = require("http-errors");
+const bcrypt = require("bcryptjs");
+
+const login = async (req, res, next) => {
+  const { password, email } = req.body;
+  const user = await service.getUserByEmail(email);
+  if (!user) {
+    next(createError(401, "Email or password is wrong"));
+    return;
+  }
+  const comparePassword = await bcrypt.compare(password, user.password);
+  if (!comparePassword) {
+    next(createError(401, "Email or password is wrong"));
+    return;
+  }
+  const payload = {
+    id: user.id,
+    email: user.email,
+  };
+  const updatedUser = await service.addToken(payload);
+  res.status(200).json({
+    token: updatedUser.token,
+    user: {
+      email: updatedUser.email,
+      subscription: updatedUser.subscription,
+    },
+  });
+};
+
+module.exports = login;
