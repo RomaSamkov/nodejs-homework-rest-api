cfb8fa839e3386c3cf9f4d84265d972a91095875
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-05 23:00:44 +0300

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,20 @@
 const express = require("express");
-const { schemaValidation } = require("../../middlewares/userValidation");
+const { validationMiddleware } = require("../../middlewares/userValidation");
+const { authMiddleware } = require("../../middlewares/tokenValidation");
 const handleCatchErrors = require("../../middlewares/errorHandler");
 
 const { users: ctrl } = require("../../controllers");
 
 const router = express.Router();
 
-router.post("/signup", schemaValidation, handleCatchErrors(ctrl.register));
+router.post("/signup", validationMiddleware, handleCatchErrors(ctrl.register));
+
+router.post("/login", validationMiddleware, handleCatchErrors(ctrl.login));
+
+router.get("/logout", authMiddleware, handleCatchErrors(ctrl.logout));
+
+router.get("/current", authMiddleware, handleCatchErrors(ctrl.getCurrent));
+
+router.patch("/:userId", handleCatchErrors(ctrl.updateSubscription));
 
 module.exports = router;
