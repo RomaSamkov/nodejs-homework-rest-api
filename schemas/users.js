0ea9c2e42d6699aa4c0f2433c95937d6a37c9364
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-06 22:12:30 +0300

    Update users.js

--- schemas/users.js
+++ schemas/users.js
@@ -2,8 +2,11 @@ const Joi = require("joi");
 
 const userSchema = Joi.object({
   password: Joi.string().alphanum().min(5).required(),
-  email: Joi.string().email().required(),
-  subscription: Joi.string().allow("starter", "pro", "business"), // or pattern(/^(starter|pro|business)$/)
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  subscription: Joi.string().allow("starter", "pro", "business"),
   token: Joi.string(),
 });
 
