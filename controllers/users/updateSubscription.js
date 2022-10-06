0ad9fb58bb805d16f57f64cd0aa3feb378bba467
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-06 22:11:56 +0300

    Update updateSubscription.js

--- controllers/users/updateSubscription.js
+++ controllers/users/updateSubscription.js
@@ -1,25 +1,12 @@
-const service = require("../../service/users");
-const createError = require("http-errors");
-const { Unauthorized } = require("http-errors");
+const { users: usersOperations } = require("../../service");
 
-const updateSub = async (req, res, next) => {
-  const { userId } = req.params;
+const updateSubscription = async (req, res) => {
+  const { _id } = req.user;
   const { subscription } = req.body;
-  const subscriptionTypes = ["starter", "pro", "business"];
-  if (!subscriptionTypes.includes(subscription)) {
-    next(createError(400, "Wrong subscription name"));
-    return;
-  }
-  const updatedResult = await service.updateSubscription(userId, subscription);
-  if (updatedResult) {
-    res.status(200).json({
-      data: {
-        user: updatedResult,
-      },
-    });
-  } else {
-    throw new Unauthorized("Unuathorized");
-  }
+
+  await usersOperations.updateSubscription(_id, subscription);
+
+  res.status(200).json({ status: "success" });
 };
 
-module.exports = updateSub;
+module.exports = updateSubscription;
