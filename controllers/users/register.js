365e90131708bef0ebbae50cbfe8838aa46f1dcb
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-06 22:11:53 +0300

    Update register.js

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,17 +1,9 @@
-const service = require("../../service/users");
-const createError = require("http-errors");
+const { users: usersOperations } = require("../../service");
 
-const registration = async (req, res, next) => {
-  const { password, email } = req.body;
-  const user = await service.getUserByEmail(email);
-  if (user) {
-    next(createError(404, "Email in use"));
-    return;
-  }
-  const newUser = await service.userSignup(password, email);
-  res.status(201).json({
-    user: { email: newUser.email, subscription: newUser.subscription },
-  });
+const register = async (req, res) => {
+  const user = await usersOperations.register(req.body);
+
+  res.status(201).json({ status: "Created", user });
 };
 
-module.exports = registration;
+module.exports = register;
