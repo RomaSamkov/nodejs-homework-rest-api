2d51fd3474482a669bb3186c63cafeb6d706ef84
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-06 22:11:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const createError = require("http-errors");
-const handleCatchErrors = require("./middlewares/errorHandler");
+const { unknownRouteHandler, errorHandler } = require("./helpers");
 
-const contactsRouter = require("./routes/api/contacts");
-const authRouter = require("./routes/api/auth");
+const { contactsRouter, authRouter } = require("./routes");
 
 const app = express();
 
@@ -18,17 +16,14 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", authRouter);
 
-app.use(
-  handleCatchErrors((req, res, next) => {
-    next(createError(404));
-  })
-);
+app.use(unknownRouteHandler);
+app.use(errorHandler);
 
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-  });
-});
+// app.use((err, req, res, next) => {
+//   res.status(err.status || 500);
+//   res.json({
+//     message: err.message,
+//   });
+// });
 
 module.exports = app;
