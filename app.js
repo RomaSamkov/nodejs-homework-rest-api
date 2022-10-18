177fee158c3ca7f5df8749374dcc65980258ea80
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-18 23:25:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const { unknownRouteHandler, errorHandler } = require("./helpers");
 
 const { contactsRouter, authRouter } = require("./routes");
+const { usersRouter } = require("./routes/api");
 
 const app = express();
 
@@ -15,7 +16,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/api/contacts", contactsRouter);
-app.use("/api/users", authRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
 
 app.use(unknownRouteHandler);
 app.use(errorHandler);
