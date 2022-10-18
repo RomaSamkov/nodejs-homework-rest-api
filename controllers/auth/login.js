561fc640c8a2e664b750e5752cc9fa7c9751c1df
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-18 23:24:49 +0300

    Create login.js

---
+++ controllers/auth/login.js
@@ -0,0 +1,10 @@
+const { auth } = require("../../service");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const { token, user } = await auth.login(password, email);
+
+  res.status(200).json({ user, token });
+};
+
+module.exports = login;
