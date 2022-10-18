c16fd26ad5174061db64652fa07cfb86fdb7dd28
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-18 23:25:01 +0300

    Create register.js

---
+++ controllers/auth/register.js
@@ -0,0 +1,9 @@
+const { auth } = require("../../service");
+
+const register = async (req, res) => {
+  const user = await auth.register(req.body);
+
+  res.status(201).json({ status: "Created", user });
+};
+
+module.exports = register;
