78b732c9000a07e3533b08de029d9e56c428decf
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-19 23:24:30 +0300

    Update index.js

--- service/users/index.js
+++ service/users/index.js
@@ -1,9 +1,13 @@
 const updateSubscription = require("./updateSubscription");
 const authUser = require("./authUser");
 const updateAvatar = require("./updateAvatar");
+const verifyEmail = require("./verifyEmail");
+const reVerifyEmail = require("./reVerifyEmail");
 
 module.exports = {
   updateSubscription,
   authUser,
   updateAvatar,
+  verifyEmail,
+  reVerifyEmail,
 };
