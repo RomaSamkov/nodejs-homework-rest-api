d76e79b6a1e9f8e163c7ee75c969387f9b05811e
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-19 23:24:34 +0300

    Update register.js

--- service/auth/register.js
+++ service/auth/register.js
@@ -1,6 +1,10 @@
 const { User } = require("../../models");
-const { RequestError } = require("../../helpers");
+const { RequestError, sendMail } = require("../../helpers");
+const { v4: uuidv4 } = require("uuid");
+require("dotenv").config();
 const gravatar = require("gravatar");
+
+const { PORT } = process.env;
 
 const register = async ({ password, email, subscription }) => {
   const user = await User.findOne({ email });
@@ -8,11 +12,25 @@ const register = async ({ password, email, subscription }) => {
   if (user) {
     throw RequestError(409, `Email ${email} in use.`);
   }
-  const avatarURL = gravatar.url(email);
-  const newUser = new User({ password, email, subscription, avatarURL });
-  await newUser.save();
 
-  return newUser;
+  const avatarURL = gravatar.url(email);
+  const verificationToken = uuidv4();
+
+  const newUser = await User.create({
+    password,
+    email,
+    subscription,
+    avatarURL,
+    verificationToken,
+  });
+
+  await sendMail({
+    to: email,
+    subject: "Confirm your registration!",
+    text: `Please confirm your email adress POST http://localhost:${PORT}/api/users/verify/${verificationToken}`,
+    html: `<a target="_blank" href="http://localhost:${PORT}/api/users/verify/${verificationToken}">confirm your verification</a`,
+  });
+  return { email: newUser.email, subscription: newUser.subscription };
 };
 
 module.exports = register;
