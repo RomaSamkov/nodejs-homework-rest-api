756892823a513c91a7dc1848f659572a65b03d61
RomaSamkov <Romansamkov.rs@gmail.com>
2022-10-20 17:12:44 +0300

    Update index.js

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,9 +1,13 @@
 const getCurrent = require("./getCurrent");
 const updateSubscription = require("./updateSubscription");
 const updateAvatar = require("./updateAvatar");
+const verifyEmail = require("./verifyEmail");
+const reVerifyEmail = require("./reVerifyEmail");
 
 module.exports = {
   getCurrent,
   updateSubscription,
   updateAvatar,
+  verifyEmail,
+  reVerifyEmail,
 };
